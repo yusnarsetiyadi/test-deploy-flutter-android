8548bee8e90f8570ea7d1f65fae46408164daf78
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2024-11-24 17:38:14 +0700

    test github actions #3

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app_with_api/screens/todo_list.dart';
+import './screens/homepage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: TodoListPage(),
+      home: Homepage(),
     );
   }
 }
